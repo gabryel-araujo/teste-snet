c7397ba7fc1aee10740465b6f5dbb0ff4145c088
gabryel <gabryelaraujo71@gmail.com>
2025-06-16 21:39:59 -0300

    feat: componente action button criado

---
+++ components/ActionButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="bg-emerald-500 p-2 rounded-lg text-slate-100 font-bold hover:shadow-md hover:cursor-pointer hover:bg-emerald-600 transition text-sm inline-flex"
+  >
+    <slot />
+  </button>
+</template>
