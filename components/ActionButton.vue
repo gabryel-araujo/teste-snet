6c02ffee24fc6b4dff064cb525b51ca8daa731d4
gabryel <gabryelaraujo71@gmail.com>
2025-06-16 23:20:59 -0300

    fix: mudança no nome do componente button

--- components/ActionButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button
-    class="bg-emerald-500 p-2 rounded-lg text-slate-100 font-bold hover:shadow-md hover:cursor-pointer hover:bg-emerald-600 transition text-sm inline-flex"
-  >
-    <slot />
-  </button>
-</template>
