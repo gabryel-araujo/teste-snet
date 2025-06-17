6b9e72e8dd214ccc834ad7ad40dba4c706bbdf20
gabryel-true <gabryelaraujo71@gmail.com>
2025-06-17 16:14:36 -0300

    feat: criação do componente Modal

---
+++ components/Modal.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { ref } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="$emit('close')" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/65 blur-2xl" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-900 p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-white"
+              >
+                {{ title }}
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  {{ subtitle }}
+                </p>
+              </div>
+
+              <div class="text-white mt-4 flex flex-col w-full">
+                <slot />
+              </div>
+
+              <div class="mt-4 w-full flex justify-end gap-2">
+                <DefaultButton
+                  @click="$emit('close')"
+                  class="bg-red-500 hover:bg-red-600"
+                >
+                  <p class="text-gray-200">Cancelar</p>
+                </DefaultButton>
+                <DefaultButton @click="$emit('close')">
+                  Cadastrar
+                </DefaultButton>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
