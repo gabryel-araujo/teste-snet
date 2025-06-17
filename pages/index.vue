c31be6f4bd9f53bbd9c76486835126a69eb2fb4e
gabryel <gabryelaraujo71@gmail.com>
2025-06-16 21:43:13 -0300

    feat: utilização do componente companyCard

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,30 @@
+<!-- todo: aqui vai ser a tela de login -->
+<script setup>
+const companies = ref([
+  {
+    name: "Vivara",
+    number: 1234,
+  },
+  {
+    name: "PicoMais",
+    number: 4321,
+  },
+  {
+    name: "Snet",
+    number: 1423,
+  },
+]);
+</script>
+
 <template>
   <div>
-    <h1>Testando a main</h1>
-    <Componente>
-      <p>Esse é um componente</p>
-    </Componente>
+    <Home>
+      <CompanyCard
+        v-for="(company, index) in companies"
+        :key="index"
+        :companyName="company.name"
+        :companyNumber="company.number"
+      />
+    </Home>
   </div>
 </template>
