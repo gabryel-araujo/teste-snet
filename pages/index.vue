f104792c851bce0eedbf1ad465887a05cc865a90
gabryel <gabryelaraujo71@gmail.com>
2025-06-16 23:24:26 -0300

    config: mudança no escopo das paginas

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,11 @@
-<!-- todo: aqui vai ser a tela de login -->
 <script setup>
-const companies = ref([
-  {
-    name: "Vivara",
-    number: 1234,
-  },
-  {
-    name: "PicoMais",
-    number: 4321,
-  },
-  {
-    name: "Snet",
-    number: 1423,
-  },
-]);
+definePageMeta({
+  layout: "login",
+});
 </script>
 
 <template>
-  <div>
-    <Home>
-      <CompanyCard
-        v-for="(company, index) in companies"
-        :key="index"
-        :companyName="company.name"
-        :companyNumber="company.number"
-      />
-    </Home>
+  <div class="flex justify-center items-center flex-1 p-4">
+    <LoginForm />
   </div>
 </template>
