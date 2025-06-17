8571a4df545e410630d7cb21a7f1b05b5b346dfc
gabryel <gabryelaraujo71@gmail.com>
2025-06-16 21:40:57 -0300

    style: estilização final do nav e do footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full bg-emerald-600 flex items-center justify-center">
-    <h1 class="text-white font-bold">Desenvolvido por Gabryel Araújo</h1>
+  <div class="w-full bg-slate-900 flex items-center justify-center h-20">
+    <h1 class="font-bold text-emerald-400">Snet 2025 &copy;</h1>
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="w-full bg-emerald-600 flex items-center justify-center">
-    <h1 class="text-white font-bold">Header</h1>
+  <div
+    class="w-full bg-slate-900 flex items-center justify-between h-20 gap-2 text-emerald-400 font-semibold p-4"
+  >
+    <section class="flex items-center gap-2">
+      <img
+        src="/public/favicon.ico"
+        alt="ícone de dois triangulos verdes sobrepostos"
+      />
+      <h1 class="font-bold">Snet Test</h1>
+    </section>
+    <section class="flex items-center gap-6 text-white">
+      <p class="text-emerald-400">Home</p>
+      <p>Empresas</p>
+      <section class="bg-white rounded-full p-2">
+        <User class="text-emerald-400" />
+      </section>
+    </section>
   </div>
 </template>
+
+<script setup>
+import { User } from "lucide-vue-next";
+</script>
