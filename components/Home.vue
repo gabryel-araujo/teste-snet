631f7b9d1e84cf4d70a4c864538b07dc943d7f7b
gabryel-true <gabryelaraujo71@gmail.com>
2025-06-17 16:14:49 -0300

    feat: ajustes na home para receber o modal

--- components/Home.vue
+++ components/Home.vue
@@ -3,13 +3,18 @@
     <div class="flex flex-col gap-4">
       <p class="text-2xl font-bold">Suas Organizações</p>
       <div class="flex gap-2">
-        <DefaultButton><Plus /> Nova organização </DefaultButton>
-        <!-- todo: criar um componente com esse input -->
-        <input
-          type="search"
-          placeholder="Busque aqui..."
-          class="border border-gray-600 rounded-md p-1 focus:ring-emerald-400 focus:ring-1 outline-none text-sm w-1/4"
-        />
+        <DefaultButton @click="isOpen = true"
+          ><Plus /> Nova organização
+        </DefaultButton>
+        <Modal
+          :isOpen="isOpen"
+          @close="isOpen = false"
+          :title="'Cadastrar Empresa'"
+          :subtitle="'Preencha o formulário abaixo com os dados da nova empresa'"
+        >
+          <CompanyForm />
+        </Modal>
+        <DefaultInput :type="'search'" :placeholder="'Digite aqui'" />
       </div>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -20,4 +25,6 @@
 
 <script setup>
 import { Plus } from "lucide-vue-next";
+import { ref } from "vue";
+const isOpen = ref(false);
 </script>
