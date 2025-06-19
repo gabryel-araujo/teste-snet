a380a2e4dab524e32e1aa2f732aa32d0a9dfdddb
gabryel-true <gabryelaraujo71@gmail.com>
2025-06-19 11:20:36 -0300

    feat: modificação de props para os componentes

--- components/DefaultButton.vue
+++ components/DefaultButton.vue
@@ -1,5 +1,15 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
 <template>
   <button
+    :type="props.type"
     class="bg-emerald-500 p-2 rounded-lg text-slate-100 font-bold hover:shadow-md hover:cursor-pointer hover:bg-emerald-600 transition text-sm inline-flex items-center justify-center"
   >
     <slot />
