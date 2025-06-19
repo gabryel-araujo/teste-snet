1d6fa556d43face0eec77a74fbd94319b6a50585
gabryel-true <gabryelaraujo71@gmail.com>
2025-06-19 15:55:31 -0300

    fix: ajustes na página home

--- components/Home.vue
+++ components/Home.vue
@@ -1,23 +1,32 @@
-<script setup>
+<script setup lang="ts">
 import { Plus } from "lucide-vue-next";
 import { ref } from "vue";
+import type { Estabelecimentos } from "~/types/Estabelecimentos";
 const isOpen = ref(false);
 
-const props = defineProps({
-  company: {
-    type: Object,
-    required: true,
-  },
-});
+const emit = defineEmits(["submitted"]);
+
+const props = defineProps<{
+  company: Estabelecimentos;
+}>();
+
+function sendData() {
+  emit("submitted");
+}
 </script>
 
 <template>
   <div class="h-full p-4 flex flex-col gap-4">
     <div class="flex flex-col gap-4">
-      <p class="text-2xl font-bold" v-if="company">
-        {{ company.name }} - {{ company.number }}
-      </p>
-      <p class="text-2xl font-bold" v-else>Suas Organizações</p>
+      <section>
+        <p class="text-2xl font-bold" v-if="company">
+          {{ props.company.nome }} - {{ props.company.numero_estabelecimento }}
+        </p>
+        <p class="text-md text-gray-500" v-if="company">
+          {{ props.company.razao_social }}
+        </p>
+        <p class="text-2xl font-bold" v-else>Suas Organizações</p>
+      </section>
       <div class="flex gap-2" v-if="company == null">
         <DefaultButton @click="isOpen = true"
           ><Plus /> Nova organização
@@ -28,7 +37,15 @@ const props = defineProps({
           :title="'Cadastrar Empresa'"
           :subtitle="'Preencha o formulário abaixo com os dados da nova empresa'"
         >
-          <CompanyForm />
+          <CompanyForm
+            :isOpen="isOpen"
+            @submitted="
+              () => {
+                sendData();
+                isOpen = false;
+              }
+            "
+          />
         </Modal>
         <DefaultInput :type="'search'" :placeholder="'Digite aqui'" />
       </div>
